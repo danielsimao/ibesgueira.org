e6d3289c1dbf49f1f8ea41c9b186dea6fcc706ed
Rui Simao <rui.simao@hostelworld.com>
2021-01-20 09:09:41 +0000

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../utils/gtag'
 
 function MyApp({
   Component,
@@ -8,6 +11,17 @@ function MyApp({
   Component: () => JSX.Element
   pageProps
 }): JSX.Element {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <>
       <Head>
